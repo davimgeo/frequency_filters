498fcf426edba03d8d95dd1661e3d7870bf00f43
davimgeo <davi.joestar02@gmail.com>
2024-08-07 14:51:58 -0300

    add functions to script

--- execute_program.sh
+++ execute_program.sh
@@ -4,32 +4,38 @@ compiled_name="filtered_wavelet"
 functions=(functions/generate_wavelet.cpp functions/fft_algorithms.cpp functions/frequency_filters.cpp)
 temp_files=(wavelet.txt fft_result.txt filtered_fft_result.txt ifft_result.txt parameters_file.txt)
 
-echo "Compiling C++ file..."
-g++ main.cpp "${functions[@]}" -o $compiled_name -lfftw3
-if [ $? -ne 0 ]; then
-    echo "Compilation failed."
-    exit 1
-fi
+compile_cpp_file()
+{
+    echo "Compiling C++ file..."
+    g++ main.cpp "${functions[@]}" -o $compiled_name -lfftw3
+    if [ $? -ne 0 ]; then
+        echo "Compilation failed."
+        exit 1
+    fi
+}
 
-echo "Executing C++ file..."
-./$compiled_name
+execute_cpp_file()
+{
+   echo "Executing C++ file..."
+    ./$compiled_name
+    if [ $? -ne 0 ]; then
+        echo "C++ file execution failed."
+        exit 1
+    fi 
+}
 
-if [ $? -ne 0 ]; then
-    echo "C++ file execution failed."
-    exit 1
-fi
-
-echo "Executing Python file..."
-python3 wavelet_plot.py
-if [ $? -ne 0 ]; then
-    echo "Python file execution failed."
-    exit 1
-fi
-
-echo "Cleaning temporary files..."
+execute_python_file()
+{
+    python3 wavelet_plot.py
+    if [ $? -ne 0 ]; then
+        echo "Python file execution failed."
+        exit 1
+    fi    
+}
 
 remove_temp_files() 
 {
+    echo "Cleaning temporary files..."
     for temp_file in "${temp_files[@]}"
     do
         if [ -f "$temp_file" ]; then
@@ -38,6 +44,9 @@ remove_temp_files()
     done
 }
 
+compile_cpp_file
+execute_cpp_file
+execute_python_file
 remove_temp_files
 
 rm $compiled_name
